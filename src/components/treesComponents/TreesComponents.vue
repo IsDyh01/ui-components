1d553150918156d11168da05ecb8caac9756d6db
yihao_dang <1335902682@qq.com>
2024-01-09 23:10:22 +0800

    feat: 树形组件初始化完成

--- src/components/treesComponents/TreesComponents.vue
+++ src/components/treesComponents/TreesComponents.vue
@@ -1,5 +1,60 @@
 <template>
-  <div>111</div>
+  <div class="tree-components">
+    <div v-for="(item, idx) in data" :key="item.id">
+      <div class="item">
+        <div
+          class="serial"
+          :style="{ textIndent: `${(item.level - 1) * 20}px` }"
+          v-if="item.level === 1"
+        >
+          {{ oneLevelToString[idx] }}
+        </div>
+        <div
+          class="serial"
+          :style="{ textIndent: `${(item.level - 1) * 20}px` }"
+          v-else
+        >
+          {{ item.serial }}
+        </div>
+        <div class="title">{{ item.title }}</div>
+      </div>
+      <TreesComponents
+        :data="item.children"
+        v-if="item.children?.length"
+      ></TreesComponents>
+    </div>
+  </div>
 </template>
-<script lang="ts" setup></script>
-<style lang="scss" scoped></style>
+<script lang="ts" setup>
+import TreesComponents from "./TreesComponents.vue";
+interface DataType {
+  level: number; // 几级标题 1 2 3
+  serial: string; // 序号
+  title: string; // 标题名称
+  children?: DataType[]; // 子标题
+  id: number;
+}
+const props = withDefaults(
+  defineProps<{
+    isDynamic?: boolean; // 是否支持动态增加 | 删除
+    data: DataType[]; // 数据项数组
+  }>(),
+  {
+    isDynamic: false,
+  }
+);
+
+const oneLevelToString: string[] = ["一", "二", "三"];
+</script>
+<style lang="scss" scoped>
+.tree-components {
+  .item {
+    display: flex;
+    .serial {
+    }
+    .title {
+      margin-left: 5px;
+    }
+  }
+}
+</style>
--- src/views/treesComponents/TreesComponents.vue
+++ src/views/treesComponents/TreesComponents.vue
@@ -1,5 +1,65 @@
 <template>
-  <ui-trees-components></ui-trees-components>
+  <ui-trees-components :data="data"></ui-trees-components>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+interface DataType {
+  level: number; // 几级标题 1 2 3
+  serial: string; // 序号
+  title: string; // 标题名称
+  children?: DataType[]; // 子标题
+  id: number;
+}
+const data: DataType[] = [
+  {
+    id: 1,
+    level: 1,
+    serial: "1",
+    title: "序幕1",
+    children: [
+      {
+        id: 4,
+        level: 2,
+        serial: "1.1",
+        title: "序幕1.1",
+      },
+    ],
+  },
+  {
+    id: 2,
+    level: 1,
+    serial: "2",
+    title: "序幕2",
+    children: [
+      {
+        id: 5,
+        level: 2,
+        serial: "2.1",
+        title: "序幕2.1",
+        children: [
+          {
+            id: 7,
+            level: 3,
+            serial: "2.1.1",
+            title: "序幕2.1.1",
+          },
+        ],
+      },
+    ],
+  },
+  {
+    id: 3,
+    level: 1,
+    serial: "3",
+    title: "序幕3",
+    children: [
+      {
+        id: 6,
+        level: 2,
+        serial: "3.1",
+        title: "序幕3.1",
+      },
+    ],
+  },
+];
+</script>
 <style lang="scss" scoped></style>
