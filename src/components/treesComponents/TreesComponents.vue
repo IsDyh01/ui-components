4290f289a2930ec0e13868ca095f2318bfa89a28
yihao_dang <1335902682@qq.com>
2024-01-10 23:55:35 +0800

    feat: 增加树形组件添加标题功能

--- src/components/treesComponents/TreesComponents.vue
+++ src/components/treesComponents/TreesComponents.vue
@@ -12,12 +12,17 @@
         </div>
         <div
           class="serial"
-          :style="{ textIndent: `${(item.level - 1) * 20}px` }"
+          :style="{ textIndent: `${(item.level! - 1) * 20}px` }"
           v-else
         >
           {{ item.serial }}
         </div>
         <div class="title">{{ item.title }}</div>
+        <!-- 增加 | 删除操作 -->
+        <div class="action">
+          <div class="add" @click="addItem(item)">+</div>
+          <div class="delete" @click="deleteItem">-</div>
+        </div>
       </div>
       <TreesComponents
         :data="item.children"
@@ -29,8 +34,8 @@
 <script lang="ts" setup>
 import TreesComponents from "./TreesComponents.vue";
 interface DataType {
-  level: number; // 几级标题 1 2 3
-  serial: string; // 序号
+  level?: number; // 几级标题 1 2 3
+  serial?: string; // 序号
   title: string; // 标题名称
   children?: DataType[]; // 子标题
   id: number;
@@ -46,15 +51,51 @@ const props = withDefaults(
 );
 
 const oneLevelToString: string[] = ["一", "二", "三"];
+
+// 添加数据项，如一级标题点击+是添加一级标题下的二级标题
+const addItem = (item: DataType) => {
+  item.children?.push({
+    id: +Date.now(),
+    title: "序幕",
+  });
+};
+
+const deleteItem = () => {};
 </script>
 <style lang="scss" scoped>
 .tree-components {
   .item {
     display: flex;
-    .serial {
-    }
+    align-items: center;
     .title {
       margin-left: 5px;
+    }
+    .action {
+      display: flex;
+      margin-left: 5px;
+      .add {
+        width: 15px;
+        height: 15px;
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: aqua;
+        color: white;
+        cursor: pointer;
+        margin-right: 5px;
+      }
+      .delete {
+        width: 15px;
+        height: 15px;
+        border-radius: 50%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: aqua;
+        color: white;
+        cursor: pointer;
+      }
     }
   }
 }
